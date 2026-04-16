d2f82eb5738c3c295ead93d57dad6d8c50933e5a
RiciMarino <riccardo.marino@csi-net.it>
2026-04-16 17:30:15 +0200

    Update app.js

--- app.js
+++ app.js
@@ -126,7 +126,8 @@ function renderSection(sectionKey){
 }
 
 async function init(){
-  console.log("Uso dati già presenti in app.js");
+  const res = await fetch('./data.json');
+  surveyData = await res.json();
 
   document.getElementById('generatedOn').textContent = surveyData.generated_on;
   renderSummaryCards();
